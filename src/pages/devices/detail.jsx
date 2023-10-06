504214156ed49d346300fa51d97edcea8c9a6a38
Adrian Sanchezdelc <asanchezdelc@mydevices.com>
2023-10-06 09:55:30 -0700

    fix: remove device request

--- src/pages/devices/detail.jsx
+++ src/pages/devices/detail.jsx
@@ -195,7 +195,7 @@ export default function DeviceDetail() {
               <div className='mt-8'>
                   <Credentials clientId={device.serial} />
                 </div>
-                <button onClick={onDeleteDevice} type="button" className="focus:outline-none inline-flex text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                <button onClick={() => onDeleteDevice(device._id)} type="button" className="focus:outline-none inline-flex text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                   <TrashIcon className='w-5 h-5 mr-2'/>
                   Remove Device
                </button>
