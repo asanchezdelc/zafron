6e4f2119c87fec258a2f8db394c05710bfdd6200
Adrian Sanchezdelc <asanchezdelc@mydevices.com>
2024-03-08 22:44:04 -0800

    feat: ui tweak for add device

--- src/pages/devices/add.jsx
+++ src/pages/devices/add.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
 import { Dialog } from '@headlessui/react'
-import { Button, TextInput, Select, SelectItem, Flex } from '@tremor/react';
+import { Button, TextInput, Select, SelectItem, Flex, Text } from '@tremor/react';
 import { generateDeviceName, generateSerial } from '../../services/magicwords';
+import { Link } from 'react-router-dom';
+import DeviceType from './onboarding/devicetype';
 import * as profileAPI from '../../services/profiles';
 
 export default function AddDevice({onCancel, onAction}) {
@@ -60,10 +62,10 @@ export default function AddDevice({onCancel, onAction}) {
         {/* Container to center the panel */}
         <div className="flex min-h-full items-center justify-center p-4">
         <div className="relative bg-white rounded-lg shadow">
-      <Dialog.Panel>
+      <Dialog.Panel className="w-full max-w-md">
           <div className="flex items-start justify-between p-4 border-b rounded-t">
                 <h3 className="text-xl font-semibold">
-                    Add a new device
+                    Add Device
                 </h3>
                 <button type="button" onClick={() => onCancel(false)} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="defaultModal">
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -74,32 +76,37 @@ export default function AddDevice({onCancel, onAction}) {
             </div>
         
           <form className='p-10'>
-          <div className="mb-6 w-72">
-            <label className="block mb-2 text-sm font-medium text-gray-900">Name</label>
-            <TextInput value={name} onChange={(e) => setName(e.target.value)} placeholder="Device Name" />
-          </div>
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900">Serial</label>
-            <TextInput value={serial} onChange={(e) => validateSerial(e.target.value)} placeholder="Device Serial" />
-          </div>
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900">Type</label>
-            <Select value={type} onValueChange={setType} defaultValue='mqtt'>
-              <SelectItem value="mqtt">MQTT</SelectItem>
-              <SelectItem value="lora">LoRaWAN</SelectItem>
-              <SelectItem value="http">HTTP</SelectItem>
-            </Select>
-          </div>
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900">Source</label>
+            <DeviceType onChange={setType} selected={type} />
+            <div className="mb-6">
+              <label className="block mb-2 text-sm font-medium text-gray-900">Name</label>
+              <TextInput value={name} onChange={(e) => setName(e.target.value)} placeholder="Device Name" />
+            </div>
+            <div className="mb-6">
+              <label className="block mb-2 text-sm font-medium text-gray-900">
+                { type === 'lora' ? 'Device EUI' : 'Serial'}
+              </label>
+              <TextInput value={serial} onChange={(e) => validateSerial(e.target.value)} placeholder="Device Serial" />
+            </div>
+            {/* <div className="mb-6">
+              <label className="block mb-2 text-sm font-medium text-gray-900">Type</label>
+              <Select value={type} onValueChange={setType} defaultValue='mqtt'>
+                <SelectItem value="mqtt">MQTT</SelectItem>
+                <SelectItem value="lora">LoRaWAN</SelectItem>
+                <SelectItem value="http">HTTP</SelectItem>
+              </Select>
+            </div> */}
             { (type === 'http' || type === 'lora') &&  (
-            <Select value={profile} onValueChange={setProfile}>
-              { profiles.map((profile) => (
-                <SelectItem value={profile._id}>{profile.name}</SelectItem>
-              ))}
-            </Select>)
-            }
-          </div>
+              <div className="mb-6">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Select Profile</label>
+                <Select value={profile} onValueChange={setProfile}>
+                  { profiles.map((profile) => (
+                    <SelectItem value={profile._id}>{profile.name}</SelectItem>
+                  ))}
+                </Select>
+                <Text className='mt-1 mr-2'>Don't see a profile? <Link to="/profiles" className="text-blue-500">Create Profile</Link></Text>
+                </div>)
+              }
+            
             <Flex justifyContent="end" className="space-x-2 border-t pt-4 mt-8">
               <Button variant="secondary" size='xs' onClick={() => onCancel(false)}>Cancel</Button>
               <Button variant="primary" size='xs' onClick={onAddDevice} className='mr-6'>Add Device</Button>
